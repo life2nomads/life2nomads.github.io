b6a1cb9836233544bdba060447c76730206c2620
Alymzhan Kurbanov <alymzhank@gmail.com>
2025-01-15 22:52:10 -0500

    Added new web page

--- misc/_.js
+++
@@ -1,9 +0,0 @@
-const flatten = (array) => {
-  return array.reduce((a, b) => a.concat(b), []);
-};
-
-const toSet = (array) => {
-  return [ ...new Set(array) ];
-};
-
-export { flatten, toSet };
--- misc/constants.js
+++
@@ -1,2 +0,0 @@
-export const SITE_TITLE = 'MAP-CY';
-export const SITE_DOMAIN = 'map-cy.com';
